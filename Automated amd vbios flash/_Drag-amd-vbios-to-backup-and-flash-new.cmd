69d453aa7843c29ddb5888d75f09fd739cb1e7ed
Vasily Galkin <galkin-vv@ya.ru>
2018-01-26 11:03:47 +0300

    time of backup

--- Automated amd vbios flash/_Drag-amd-vbios-to-backup-and-flash-new.cmd
+++ Automated amd vbios flash/_Drag-amd-vbios-to-backup-and-flash-new.cmd
@@ -2,5 +2,7 @@
 @if not exist "%~f1" echo "vbios file %~nx1 in the folder %~dp1 can't be used, place it in %~dp0 and try again" && pause && exit
 @set amd_vbios_cmdline="%~f0" "%~f1" --no-elevate
 @if x%2==x start "" "%~dp0\run-amd-vbios-as-admin.lnk" && exit
+@set now=%date:.=-%_%time::=%
+@set amd_vbios_backup=amd_vbios_backup-%now:~0,17%.rom
 @call "%~dp0\atiflash.exe" -ai 0
 @echo Finished. Window can be closed
