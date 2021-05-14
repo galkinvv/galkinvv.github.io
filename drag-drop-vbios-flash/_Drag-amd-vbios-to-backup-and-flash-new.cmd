c2ef2416ea7b7766c0f0676c0bda4938b0bed281
Vasily Galkin <galkin-vv@ya.ru>
2021-05-14 13:59:05 +0300

    Update _Drag-amd-vbios-to-backup-and-flash-new.cmd
    Handle pressing no in eleveation dialog by exit (not tested in win10)

--- drag-drop-vbios-flash/_Drag-amd-vbios-to-backup-and-flash-new.cmd
+++ drag-drop-vbios-flash/_Drag-amd-vbios-to-backup-and-flash-new.cmd
@@ -1,7 +1,7 @@
 @if x%1==x echo Drag a rom file over the shortcut to flash amd vbios && pause && exit
 @if not exist "%~f1" echo "vbios file %~nx1 in the folder %~dp1 can't be used, place it in %~dp0 and try again" && pause && exit
 @set run_as_admin_cmdline="%~f0" "%~f1" --no-elevate
-@if x%2==x start "" "%~dp0\run_as_admin_cmdline.lnk" && exit
+@if x%2==x start "" "%~dp0\run_as_admin_cmdline.lnk" && exit || exit
 @set now=%date:.=-%_%time::=%
 @set now=%now: =%
 @set now=%now:,=%
