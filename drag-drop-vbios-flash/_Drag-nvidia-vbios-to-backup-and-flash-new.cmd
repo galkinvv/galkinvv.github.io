6b63691a2af28fc5d6fffa348448c6bcc227a810
Vasily Galkin <galkin-vv@ya.ru>
2021-06-10 10:07:31 -0700

    amd-like fixes

--- drag-drop-vbios-flash/_Drag-nvidia-vbios-to-backup-and-flash-new.cmd
+++ drag-drop-vbios-flash/_Drag-nvidia-vbios-to-backup-and-flash-new.cmd
@@ -1,7 +1,7 @@
 @if x%1==x echo Drag a rom file over the shortcut to flash nvidia vbios && pause && exit
 @if not exist "%~f1" echo "vbios file %~nx1 in the folder %~dp1 can't be used, place it in %~dp0 and try again" && pause && exit
 @set run_as_admin_cmdline="%~f0" "%~f1" --no-elevate
-@if x%2==x start "" "%~dp0\run_as_admin_cmdline.lnk" && exit
+@if x%2==x start "" "%~dp0\run_as_admin_cmdline.lnk" && exit || exit
 @set now=%date:.=-%_%time::=%
 @set now=%now: =%
 @set now=%now:,=%
@@ -12,5 +12,5 @@
 @set flasher=nvflash.exe
 @if not exist %flasher% set flasher=nvflash64.exe
 call %flasher% --save %backup_name%
-call %flasher% -6 %~f1
+call %flasher% -6 "%~f1"
 @echo Finished. Window can be closed
